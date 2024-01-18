f5b1214a376a60511bc7b0c658bb8c0c990d9852
jorgehernandezlp <jorgehernandezlp@gmail.com>
2024-01-18 20:49:00 +0000

    Inicio de proyecto

---
+++ src/test/java/es/dsw/app/PruebaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dsw.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
